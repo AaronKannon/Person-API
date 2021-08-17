dd616775d38c18d74a03e3941c607bca90c29bd4
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-08-17 15:07:26 -0300

    Method to list all people in database

--- src/main/java/kannon/aaron/personadminapi/service/PersonService.java
+++ src/main/java/kannon/aaron/personadminapi/service/PersonService.java
@@ -8,6 +8,9 @@ import kannon.aaron.personadminapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
@@ -26,4 +29,8 @@ public class PersonService {
         return MessageResponseDTO.builder().message("Person with ID " + savedPerson.getId() + " with success").build();
     }
 
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
+    }
 }
