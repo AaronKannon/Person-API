7d9820fb036a1a36ebe263720ee3ee6920e69e12
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-08-17 17:00:53 -0300

    Adding the annotation JsonIgnore to the Entity Address to fix the GET errors

--- src/main/java/kannon/aaron/personadminapi/entity/Address.java
+++ src/main/java/kannon/aaron/personadminapi/entity/Address.java
@@ -1,5 +1,6 @@
 package kannon.aaron.personadminapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import kannon.aaron.personadminapi.enums.ResidenceType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import javax.persistence.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Address {
 
     @Id
