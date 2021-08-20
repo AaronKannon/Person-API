4ecbba6692cc8b9eb6086af2c4ea6eab431316b0
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-08-20 18:10:11 -0300

    Added the Swagger to generate the API documentation and updated READ.me file

---
+++ src/main/java/kannon/aaron/personadminapi/swagger/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package kannon.aaron.personadminapi.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+//@EnableOpenApi
+public class SwaggerConfig /*extends WebMvcConfigurationSupport*/ {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("kannon.aaron"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Person API")
+                .description("API-REST for people management inside a college")
+                .version("0.0.1")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("AaronKannon", "https://github.com/AaronKannon", "wellingtonbmoraes@hotmail.com"))
+                .build();
+    }
+}
